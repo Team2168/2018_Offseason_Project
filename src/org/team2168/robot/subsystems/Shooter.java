7523507a1e2be743b9698d0d102056422914db74
James <james@team2168.org>
2017-01-14 22:22:01 -0500

    Moved robot package to org.team2168 like we've done in the past. Removed "West Coast Drive" sub-directory. Project is in top level directory.

---
+++ src/org/team2168/robot/subsystems/Shooter.java
@@ -0,0 +1,48 @@
+package org.team2168.robot.subsystems;
+
+import org.team2168.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Subsystem class for the Shooter
+ */
+public class Shooter extends Subsystem {
+
+	Victor ShooterMotor;
+	
+	Encoder ShooterEncoder;
+	
+	private static Shooter instance = null;
+	
+	public Shooter() {
+		
+		ShooterMotor = new Victor(RobotMap.SHOOTER_MOTOR);
+
+		ShooterEncoder = new Encoder(RobotMap.SHOOTER_ENCODER_A, RobotMap.SHOOTER_ENCODER_B);
+		
+	}
+	
+	public static Shooter getInstance(){
+		if(instance == null)
+			instance = new Shooter();
+		
+		return instance;
+	}
+	
+	public void driveShooterMotor(double speed) {
+		ShooterMotor.set(speed);
+	}
+	
+	public double getShooterEncoder() {
+		return ShooterEncoder.get();
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
