f0baa40806f940ee58ed45afc9b3f7d48bca476c
Peter Dentch <PJDentch@gmail.com>
2017-01-18 20:27:54 -0500

    files for lift

---
+++ DriveLiftWithJoystick.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team2168.robot.lift.commands;
+
+import org.usfirst.frc.team2168.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveLiftWithJoystick extends Command {
+
+    public DriveLiftWithJoystick() {
+        // Use requires() here to declare subsystem dependencies
+        requires(Robot.lift);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double upSpeed = Robot.oi.driverJoystick.getRightTriggerAxisRaw();
+    	double downSpeed = Robot.oi.driverJoystick.getLeftTriggerAxisRaw();
+    	
+    	Robot.lift.drive(upSpeed - downSpeed);
+    	
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.lift.drive(0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
---
+++ Lift.java
@@ -0,0 +1,55 @@
+package org.usfirst.frc.team2168.robot.subsystems;
+
+import org.usfirst.frc.team2168.robot.RobotMap;
+import org.usfirst.frc.team2168.robot.lift.commands.DriveLiftWithJoystick;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Lift extends Subsystem {
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	
+	private Victor liftMotor1;
+	private Victor liftMotor2;
+	
+	
+	static Lift instance = null;
+
+	
+	private Lift(){
+		
+		liftMotor1 = new Victor(RobotMap.LIFT_MOTOR_1);
+		liftMotor2 = new Victor(RobotMap.LIFT_MOTOR_2);
+	}
+	
+	public static Lift getInstance(){
+		
+		if(instance == null)
+			instance = new Lift();
+		
+		return instance;
+	}
+	
+	public void drive(double speed){
+		
+		if(RobotMap.REVERSE_LIFT_MOTORS){
+			speed = -speed;
+			
+		}
+		
+		liftMotor1.set(speed);
+		liftMotor2.set(-speed);
+		
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+    	setDefaultCommand(new DriveLiftWithJoystick());
+    }
+}
+
