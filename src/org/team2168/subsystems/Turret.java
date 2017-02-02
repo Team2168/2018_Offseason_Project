917058167503a9a40e0b61450e0db072b2949efa
benmw <benmw@DESKTOP-SFK4FCC.groton.k12.ct.us>
2017-02-01 21:32:47 -0500

    So we don't break the turret too hard

--- src/org/team2168/subsystems/Turret.java
+++ src/org/team2168/subsystems/Turret.java
@@ -19,15 +19,16 @@ public class Turret extends Subsystem {
 
     private static Spark TurretMotor;
     private static AnalogInput Potentiometer;
-    private static DigitalInput LimitSwitch1;
-    private static DigitalInput LimitSwitch2;
+    private static DigitalInput LimitSwitchRight;
+    private static DigitalInput LimitSwitchLeft;
 
     private static Turret instance = null;
     
     private static LinearInterpolator turretInterpolator;
     //TODO get these values plez
-    private double[][] turretRange = {{0.0,0.0},
-    		 			              {1.0,100.0}};
+    private double[][] turretRange = {{-1.0,-100.0},
+    		                          {0.0,0.0},
+    		                          {1.0,100.0}};
     
     /**
      * Default constructor for Turret subsystem
@@ -35,8 +36,8 @@ public class Turret extends Subsystem {
     private Turret() {
     	TurretMotor = new Spark(RobotMap.TURRET_MOTOR);
     	Potentiometer = new AnalogInput(RobotMap.TURRET_POTENTIOMETER);
-    	LimitSwitch1 = new DigitalInput(RobotMap.TURRET_LIMIT_SWITCH_1);
-    	LimitSwitch2 = new DigitalInput(RobotMap.TURRET_LIMIT_SWITCH_2);
+    	LimitSwitchRight = new DigitalInput(RobotMap.TURRET_LIMIT_SWITCH_1);
+    	LimitSwitchLeft = new DigitalInput(RobotMap.TURRET_LIMIT_SWITCH_2);
     	turretInterpolator = new LinearInterpolator(turretRange);
     }
     
@@ -53,10 +54,15 @@ public class Turret extends Subsystem {
 	
 	/**
 	 * Sets the speed of the motor
-	 * @param speed (0-1)
+	 * @param speed of -1.0 (left) to 1.0 (right)
 	 */
 	public void setSpeed(double speed) {
-		TurretMotor.set(speed);
+		if((speed > 0 && isLimitSwitchRightActive())||(speed < 0 && isLimitSwitchLeftActive())){
+			TurretMotor.set(0);
+		}
+		else {
+			TurretMotor.set(speed);
+		}
 	}
 	
 	/**
@@ -72,16 +78,16 @@ public class Turret extends Subsystem {
 	 * Returns status of limit switch
 	 * @return true if pressed, false if unpressed
 	 */
-	public boolean isLimitSwitch1Active() {
-		return LimitSwitch1.get();
+	public boolean isLimitSwitchRightActive() {
+		return LimitSwitchRight.get();
 	}
 	
 	/**
 	 * Returns status of limit switch
 	 * @return true if pressed, false if unpressed
 	 */
-	public boolean isLimitSwitch2Active() {
-		return LimitSwitch2.get();
+	public boolean isLimitSwitchLeftActive() {
+		return LimitSwitchLeft.get();
 	}
 	
     public void initDefaultCommand() {
