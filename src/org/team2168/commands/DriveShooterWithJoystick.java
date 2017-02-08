4ce88ae0a4d7c3072084216156add9cf98659f4d
Krystina <Krystina@KrystinasPc>
2017-02-08 18:28:54 -0500

    fixes

--- src/org/team2168/commands/DriveShooterWithJoystick.java
+++ src/org/team2168/commands/DriveShooterWithJoystick.java
@@ -21,7 +21,7 @@ public class DriveShooterWithJoystick extends Command {
 
 	    // Called repeatedly when this Command is scheduled to run
 	    protected void execute() {
-	    	Robot.shooter.driveShooter(Robot.oi.operatorJoystick.getLeftStickRaw_Y());
+	    	Robot.shooter.setShooterSpeed(Robot.oi.operatorJoystick.getLeftStickRaw_Y());
 	    }
 
 	    // Make this return true when this Command no longer needs to run execute()
